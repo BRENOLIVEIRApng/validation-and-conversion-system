d75a1881dfccd92bf7e7577d3ff6a6e980dc76d8
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2025-12-03 10:47:06 -0300

    primeiro commit

---
+++ .idea/ferramentasVLS/ferramentasVLS/src/main/java/com/ferramentas/FerramentasVlsApplication.java
@@ -0,0 +1,13 @@
+package com.ferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentasVlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentasVlsApplication.class, args);
+	}
+
+}
---
+++ .idea/ferramentasVLS/ferramentasVLS/src/test/java/com/ferramentas/FerramentasVlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferramentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerramentasVlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
